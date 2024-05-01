3a63e735ceebafb9b47c7b36711b32da749b0c13
szbilisim <iletisim@szbilisim.com.tr>
2024-05-01 21:14:59 +0300

    Variables değişkenler

---
+++ java-002-variables/src/main/java/tr/com/szbilisim/Main.java
@@ -0,0 +1,7 @@
+package tr.com.szbilisim;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ java-002-variables/src/main/java/tr/com/szbilisim/Main2.java
@@ -0,0 +1,4 @@
+package tr.com.szbilisim;
+
+public class Main2 {
+}
